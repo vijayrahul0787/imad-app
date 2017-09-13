a508cb271b15d10beb935b8d9c00c79d726dbb90
vijayrahul0787 <vijayrahul07@gmail.com>
2017-09-13 16:53:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one', function (req, res) {
-  res.send('Article request one will be served here');
+   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 app.get('/article-two', function (req, res) {
   res.send('Article request two will be served here');
